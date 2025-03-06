0850e3e9a365010f7a7d351e3ca0691158aba9c7
williamprodinger <william.prodinger@student.uts.edu.au>
2025-03-06 17:21:28 +1100

    First Server Run

--- myPWA/.database/myQuery.sql
+++ myPWA/.database/myQuery.sql
@@ -1,3 +1,2 @@
-CREATE TABLE extension(extID INTEGER NOT NULL PRIMARY KEY,name TEXT NOT
-NULL, hyperlink TEXT NOT NULL,about TEXT NOT NULL,image TEXT NOT
-NULL,language TEXT NOT NULL);
+SELECT * FROM extension;
+SELECT * FROM extension WHERE language LIKE '#BASH';
